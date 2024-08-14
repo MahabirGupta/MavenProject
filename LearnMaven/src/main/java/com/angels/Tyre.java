3a580247d7096b2df643c2d69796e7b2fa8f73ba
MahabirGupta <mahabirg81@gmail.com>
2024-08-14 22:00:59 +0800

    Create a constructor in Tyre class and used Constructor injection to get the brand of the tyre

--- LearnMaven/src/main/java/com/angels/Tyre.java
+++ LearnMaven/src/main/java/com/angels/Tyre.java
@@ -2,15 +2,21 @@ package com.angels;
 
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class Tyre {
 
     private String brand;
+
+//    using constructor injection
+    public Tyre(String brand) {
+        this.brand = brand;
+    }
 
     public String getBrand() {
         return brand;
     }
 
+//    using setter injection
     public void setBrand(String brand) {
         this.brand = brand;
     }
